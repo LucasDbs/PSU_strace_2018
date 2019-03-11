ae837814860f9bcab06d53568e7ef1540ebc76cb
Sabri-Ouaked <sabri.ouaked@epitech.eu>
2019-03-11 14:19:59 +0100

    [RESET] main

--- src/main.c
+++ src/main.c
@@ -8,47 +8,6 @@
 #include "trace.h"
 #include "parsing.h"
 
-int waitchild(pid_t pid)
-{
-        int status;
-
-        waitpid(pid, &status, 0);
-        if(WIFSTOPPED(status)) {
-                return 0;
-        }
-        else if (WIFEXITED(status)) {
-                return 1;
-        }
-        else {
-                printf("%d raised an unexpected status %d", pid, status);
-                return 1;
-        }
-}
-
-int trace(pid_t child)
-{
-        long ret = 0;
-        unsigned char primary;
-        unsigned char secondary;
-        long jmps = 0;
-
-        do {
-                ret = ptrace(PTRACE_PEEKUSER, child, 8 * RIP, NULL);
-                // printf("ret = %ld\n", ret);
-                ret = ptrace(PTRACE_PEEKTEXT, child, ret, NULL);
-                // printf("ret = %ld\n", ret);
-                primary = (unsigned)0xFF & ret;
-                secondary = ((unsigned)0xFF00 & ret) >> 8;
-                // printf("primary = %c et secondary = %c\n", primary, secondary);
-                return 0;
-                if ((primary == 0xCD && secondary == 0x80) || (primary == 0x0F && secondary == 0x05))
-                        jmps++;
-                ptrace(PTRACE_SINGLESTEP, child, NULL, NULL);
-        } while(waitchild(child) < 1);
-        printf("n=> There are %lu jumps\n", jmps);
-        return (0);
-}
-
 int main(int argc, char ** argv)
 {
         // // if (pars_command(ac, av) == 84)
@@ -56,5 +15,4 @@ int main(int argc, char ** argv)
         // test_trace(av);
         launch(argc, argv);
         return (0);
-
 }
