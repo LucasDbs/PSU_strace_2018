5dc4ccdee62dbbf59426515c7fdceb943ce5f096
Sabri-Ouaked <sabri.ouaked@epitech.eu>
2019-03-12 11:38:14 +0100

    [REWORK] child process

--- src/trace.c
+++ src/trace.c
@@ -5,6 +5,7 @@
 ** main.c
 */
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,16 +18,16 @@
 #include <sys/syscall.h>
 #include "parsing.h"
 
-int waitchild(pid_t pid) {
-        int status;
+int waitchild(pid_t pid)
+{
+        int status = 0;
+        
         waitpid(pid, &status, 0);
         if(WIFSTOPPED(status)) {
                 return 0;
-        }
-        else if (WIFEXITED(status)) {
+        } else if (WIFEXITED(status)) {
                 return 1;
-        }
-        else {
+        } else {
                 printf("%d raised an unexpected status %d", pid, status);
                 return 1;
         }
@@ -38,17 +39,21 @@ int trace(pid_t child)
         long jmps = 0;
         unsigned char primary;
         unsigned char secondary;
+        struct user_regs_struct regs;
 
         do {
                 ret = ptrace(PTRACE_PEEKUSER, child, 8 * RIP, NULL);
                 ret = ptrace(PTRACE_PEEKTEXT, child, ret, NULL);
                 primary = (unsigned)0xFF & ret;
                 secondary = ((unsigned)0xFF00 & ret) >> 8;
-                if ((primary == 0xCD && secondary == 0x80) || (primary == 0x0F && secondary == 0x05))
+                if ((primary == 0xCD && secondary == 0x80) || (primary == 0x0F && secondary == 0x05)) {
+                        ptrace(PTRACE_GETREGS, child, NULL, &regs);
+                        printf("ret = %lx  prim=%lx sec=%x rax: %lld\n", ret, primary, secondary, regs.rax);
                         jmps++;
+                }
                 ptrace(PTRACE_SINGLESTEP, child, NULL, NULL);
         } while(waitchild(child) < 1);
-        printf("n=> There are %lu jumps\n", jmps);
+        printf("=> There are %lu jumps\n", jmps);
         return (0);
 }
 
@@ -58,8 +63,12 @@ int launch(int argc, char ** argv)
 
     if(child == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        // kill(getpid(), SIGUSR1);
+        // write(1, "test", 4);
+        // kill(getpid(), SIGUSR1);
+
         execve(argv[1], argv + 1, NULL);
-        // execl("/bin/ls", "ls", NULL);
+        execl("/bin/ls", "ls", NULL);
     }
     else {
         // wait for the child to stop
